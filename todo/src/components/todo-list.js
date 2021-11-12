30b4cf8e11e1c963c7e7318bc41fcbf834b69743
Victor <VPolazau@gmail.com>
2021-11-12 16:14:36 +0300

    props

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -2,11 +2,14 @@ import React from 'react';
 import TodoListItem from './todo-list-item';
 
 const TodoList = () => {
-  const items = ['Drink Coffee', 'Build Awesome App'];
   return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+      <li>
+        <TodoListItem label='Drink Coffee' />
+      </li>
+      <li>
+        <TodoListItem important label='Build Awesome App' />
+      </li>
     </ul>
   );
 };
