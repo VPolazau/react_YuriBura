75e7064dbfa94d65641c93d5f2ce9837d4dd3762
Victor <VPolazau@gmail.com>
2021-11-18 15:58:27 +0300

    обработка событий

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,24 @@ import React from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends React.Component {
+  // constructor(){
+  //   super();
+
+  //   this.onLabelClick = () => {
+  //     console.log(`Done: ${this.props.label}`);
+  //   };
+  // }
+
+  // Такая запись сверху полностью эквивалентна записи снизу
+  // В дальнейшем будем использовать её
+  // Свойство языка называется поля классов(class fields)
+  onLabelClick = () => {
+    console.log(`Done: ${this.props.label}`);
+  };
+  //
+  //
+  //
+
   render() {
     const { label, important = false } = this.props;
 
@@ -13,22 +31,26 @@ export default class TodoListItem extends React.Component {
 
     return (
       <span className='todo-list-item'>
-        <span className='todo-list-item-label' style={style}>
+        <span
+          className='todo-list-item-label'
+          style={style}
+          onClick={this.onLabelClick}
+        >
           {label}
         </span>
-
-        <button
-          type='button'
-          className='btn btn-outline-danger btn-sm float-end'
-        >
-          <i className='fa fa-trash-o' />
-        </button>
 
         <button
           type='button'
           className='btn btn-outline-success btn-sm float-end'
         >
           <i className='fa fa-exclamation' />
+        </button>
+
+        <button
+          type='button'
+          className='btn btn-outline-danger btn-sm float-end'
+        >
+          <i className='fa fa-trash-o' />
         </button>
       </span>
     );
