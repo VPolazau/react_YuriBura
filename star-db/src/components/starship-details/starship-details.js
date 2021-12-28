82477762f7dac4c4ac746bfc88b0128fe2f31dcd
Victor <VPolazau@gmail.com>
2021-12-28 15:06:34 +0300

    remove starship-details

--- star-db/src/components/starship-details/starship-details.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './starship-details.css'
-
-const StarshipDetails = () => {
-  return <div className='container'>StarshipDetails</div>
-}
-
-export default StarshipDetails
