0a2ddce091c94147780ad8f7c7a270741ba2d212
Victor <VPolazau@gmail.com>
2022-08-15 13:43:54 +0300

    React&Redux

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,24 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { bindActionCreators, legacy_createStore as createStore } from 'redux'
+
+import Counter from './counter'
+
 import reducer from './reducer'
 import * as actions from './actions'
-import { bindActionCreators, legacy_createStore as createStore } from 'redux'
-// import { configureStore } from '@reduxjs/toolkit'
 
 const store = createStore(reducer)
 const { dispatch } = store
 
 const { inc, dec, rnd } = bindActionCreators(actions, dispatch)
 
-document.getElementById('inc').addEventListener('click', inc)
-
-document.getElementById('dec').addEventListener('click', dec)
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const payload = Math.floor(Math.random() * 10)
-  rnd(payload)
-})
-
 const update = () => {
-  document.getElementById('counter').innerHTML = store.getState()
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random() * 10)
+        rnd(value)
+      }}
+    />,
+    document.getElementById('root')
+  )
 }
+
+update()
 
 store.subscribe(update)
